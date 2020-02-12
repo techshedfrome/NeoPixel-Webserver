fd7129cdb6179d221c5430bce97cab2b7c3c2b91
Will <you@example.com>
2020-02-12 23:35:10 +0000

    initial commit

---
+++ readme.md
@@ -0,0 +1,21 @@
+# Experimenting With ESP Webserver & NeoPixel
+
+Code has been patched together from examples to experiment with options for making animated lighting effects that respond to events, data values or user actions on the network.
+
+Initially, it's just using the ESP8266 async webserver, (with mDNS to make it available on the LAN as http://esp), driving a NeoPixel ring light, toggling whether the animation is active via HTTP GET requests to a specific path.
+
+Put together by [Techshed Frome](https://techshedfrome.org).
+
+
+## Notes
+
+* Had a problem with AsyncTCP library - turned out that the ESP32 library was being pulled in rather than the ESP8266 version (listed as ESPAsyncTCP)
+  * uninstalled the ESP32 version and it built/worked
+* The intention is to read data from an InfluxDB instance to show the state of sensor data
+  * we could forward the statuses to the ESP via MQTT
+  * but it's good to have some user-control over HTTP, so probably an HTTP API will remain on the ESP (not sure if we can combine that with an MQTT listener/subscriber, but we'll give it a go!)
+
+
+## References Credits:
+* Async webserver example code: https://github.com/me-no-dev/ESPAsyncWebServer/tree/master/examples/simple_server
+* Basic light ring animation:  https://github.com/JonnyBanana/NEOPIXEL_WS2812_5050_SKETCHS/blob/master/Code/Like_Disco/Like_Disco.ino
---
+++ src/main.cpp
@@ -0,0 +1,248 @@
+#include <ESP8266mDNS.h>
+#include <Adafruit_NeoPixel.h>
+
+#include <Arduino.h>
+#ifdef ESP32
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#endif
+#include <ESPAsyncWebServer.h>
+
+AsyncWebServer server(80);
+const char *PARAM_MESSAGE = "message";
+
+#ifndef STASSID
+#define STASSID ""
+#define STAPSK ""
+#define MDNS_HOSTNAME "disco"
+#define LED_PIN 2
+#define LED_COUNT 12
+#endif
+
+const char *ssid = STASSID;
+const char *password = STAPSK;
+const char *hostname = MDNS_HOSTNAME;
+bool lightIsOn = false;
+
+const int led = 16;
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void notFound(AsyncWebServerRequest *request)
+{
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URL: ";
+  message += request->url();
+  message += "\nMethod: ";
+  message += (request->method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += request->args();
+  message += "\n";
+  for (uint8_t i = 0; i < request->args(); i++)
+  {
+    message += " " + request->argName(i) + ": " + request->arg(i) + "\n";
+  }
+  request->send(404, "text/plain", "Not found");
+  digitalWrite(led, 0);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+  strip.show();            // Turn OFF all pixels ASAP
+
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
+    Serial.printf("WiFi Failed!\n");
+    return;
+  }
+  if (MDNS.begin(hostname))
+  {
+    Serial.println("MDNS responder started");
+  }
+
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    digitalWrite(led, 1);
+    request->send(200, "text/plain", "hello from esp8266!");
+    digitalWrite(led, 0);
+  });
+
+  server.on("/gif", HTTP_GET, [](AsyncWebServerRequest *request) {
+      static const uint8_t gif[] PROGMEM = {
+          0x47, 0x49, 0x46, 0x38, 0x37, 0x61, 0x10, 0x00, 0x10, 0x00, 0x80, 0x01,
+          0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0x2c, 0x00, 0x00, 0x00, 0x00,
+          0x10, 0x00, 0x10, 0x00, 0x00, 0x02, 0x19, 0x8c, 0x8f, 0xa9, 0xcb, 0x9d,
+          0x00, 0x5f, 0x74, 0xb4, 0x56, 0xb0, 0xb0, 0xd2, 0xf2, 0x35, 0x1e, 0x4c,
+          0x0c, 0x24, 0x5a, 0xe6, 0x89, 0xa6, 0x4d, 0x01, 0x00, 0x3b};
+      char gif_colored[sizeof(gif)];
+      memcpy_P(gif_colored, gif, sizeof(gif));
+      // Set the background to a random set of colors
+      gif_colored[16] = millis() % 256;
+      gif_colored[17] = millis() % 256;
+      gif_colored[18] = millis() % 256;
+
+      request->send(200, "image/gif", gif_colored);
+    });
+
+    // Send a GET request to <IP>/get?message=<message>
+    server.on("/spin", HTTP_GET, [](AsyncWebServerRequest *request) {
+      String message;
+      // if (request->hasParam(PARAM_MESSAGE))
+      // {
+      //   message = request->getParam(PARAM_MESSAGE)->value();
+      // }
+      // else
+      // {
+      //    message = "this works as well";
+      // }
+
+      lightIsOn = !lightIsOn;
+      if (lightIsOn) {
+        message = "Disco!";
+      } 
+      else {
+        message = "No Disco :(";
+      }
+      request->send(200, "text/html", "<h1>" + message + "</h1>");
+    });
+
+    // Send a POST request to <IP>/post with a form field message set to <message>
+    server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request) {
+      String message;
+      if (request->hasParam(PARAM_MESSAGE, true))
+      {
+        message = request->getParam(PARAM_MESSAGE, true)->value();
+      }
+      else
+      {
+        message = "No message sent";
+      }
+      request->send(200, "text/plain", "Hello, POST: " + message);
+    });
+
+    server.onNotFound(notFound);
+
+    server.begin();
+    Serial.println("HTTP server started");
+}
+
+void theaterChase(uint32_t color, int wait)
+{
+  for (int a = 0; a < 10; a++)
+  {
+    for (int b = 0; b < 3; b++)
+    {
+      strip.clear();
+
+      for (int c = b; c < strip.numPixels(); c += 3)
+      {
+        strip.setPixelColor(c, color);
+      }
+      strip.show();
+      delay(wait);
+    }
+  }
+}
+
+void theaterChaseTwo(uint32_t color, int wait)
+{
+  for (int a = 0; a < 5; a++)
+  {
+    for (int b = 0; b < 4; b++)
+    {
+      strip.clear();
+
+      for (int c = b; c < strip.numPixels(); c += 3)
+      {
+        strip.setPixelColor(c, color);
+      }
+      strip.show();
+      delay(wait);
+    }
+  }
+}
+
+void theaterChaseThree(uint32_t color, int wait)
+{
+  for (int a = 0; a < 10; a++)
+  {
+    for (int b = 0; b < 2; b++)
+    {
+      strip.clear();
+
+      for (int c = b; c < strip.numPixels(); c += 3)
+      {
+        strip.setPixelColor(c, color);
+      }
+      strip.show();
+      delay(wait);
+    }
+  }
+}
+
+void disco()
+{
+  strip.clear();
+  strip.show();
+  if (lightIsOn)
+    theaterChaseTwo(strip.Color(127, 127, 127), 100); // set brightness to 100
+  if (lightIsOn)
+    theaterChaseTwo(strip.Color(255, 255, 0), 100);
+  if (lightIsOn)
+    theaterChaseTwo(strip.Color(127, 0, 0), 100);
+  if (lightIsOn)
+    theaterChaseTwo(strip.Color(0, 255, 0), 100);
+  if (lightIsOn)
+    theaterChaseTwo(strip.Color(0, 0, 127), 100);
+  if (lightIsOn)
+    theaterChaseTwo(strip.Color(143, 0, 255), 100);
+
+  if (lightIsOn)
+    theaterChase(strip.Color(127, 127, 127), 50); // 50 = half brightness
+  if (lightIsOn)
+    theaterChase(strip.Color(255, 255, 0), 50);
+  if (lightIsOn)
+    theaterChase(strip.Color(127, 0, 0), 50);
+  if (lightIsOn)
+    theaterChase(strip.Color(0, 255, 0), 50);
+  if (lightIsOn)
+    theaterChase(strip.Color(0, 0, 127), 50);
+  if (lightIsOn)
+    theaterChase(strip.Color(143, 0, 255), 50);
+
+  if (lightIsOn)
+    theaterChase(strip.Color(127, 127, 127), 25); // set brightness to 25
+  if (lightIsOn)
+    theaterChase(strip.Color(255, 255, 0), 25);
+  if (lightIsOn)
+    theaterChase(strip.Color(127, 0, 0), 25);
+  if (lightIsOn)
+    theaterChase(strip.Color(0, 255, 0), 25);
+  if (lightIsOn)
+    theaterChase(strip.Color(0, 0, 127), 25);
+  if (lightIsOn)
+    theaterChase(strip.Color(143, 0, 255), 25);
+}
+
+
+void loop()
+{
+  MDNS.update();
+  disco();
+}
